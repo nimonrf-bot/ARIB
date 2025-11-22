391d714e22eea633a92f3b138082af3f01cab2ad
Nimon RF <Nimonrf@gmail.com>
2025-11-22 14:22:04 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,13 @@
 import { getApp, getApps, initializeApp, type FirebaseOptions } from 'firebase/app';
 
-const firebaseConfigStr = process.env.NEXT_PUBLIC_FIREBASE_CONFIG;
-
-if (!firebaseConfigStr) {
-  throw new Error("Missing Firebase config. Please check your .env.local file.");
-}
-
-const firebaseConfig: FirebaseOptions = JSON.parse(firebaseConfigStr);
+const firebaseConfig: FirebaseOptions = {
+  "projectId": "studio-7312804639-a23ef",
+  "appId": "1:1000253232900:web:24771e4427287986886251",
+  "apiKey": "AIzaSyA1eMgASl2hL0DKkW5jpo48otR_Og9iZZA",
+  "authDomain": "studio-7312804639-a23ef.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1000253232900"
+};
 
 
 // Initialize Firebase
