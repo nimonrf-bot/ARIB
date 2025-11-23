3c3fa3a005a4cf9b84be51e4ef2c7fc590a9d1ed
Nimon RF <Nimonrf@gmail.com>
2025-11-23 13:28:15 +0000

    File changes

--- src/lib/admins.ts
+++ src/lib/admins.ts
@@ -3,12 +3,14 @@
 
 // Users who can access the Warehouse Admin panel
 export const WAREHOUSE_ADMINS: string[] = [
+    'nimonrf@gmail.com',
     // 'warehouse.admin1@example.com',
     // 'warehouse.admin2@example.com',
 ];
 
 // Users who can access the Vessel Admin panel
 export const VESSEL_ADMINS: string[] = [
+    'nimonrf@gmail.com',
     // 'vessel.admin1@example.com',
     // 'vessel.admin2@example.com',
 ];
