03e9de89b200e8cd6b9e5845b1b2caee28ae372a
Nimon RF <Nimonrf@gmail.com>
2025-11-23 14:40:04 +0000

    it still doesnt show the logo but a simple A instead

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="ARIB" />
+        <link rel="apple-touch-icon" href="/logo.png" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#E6F3FF" />
