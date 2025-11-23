ab9ec36b8e157be66666245657f37122cb36eea3
Nimon RF <Nimonrf@gmail.com>
2025-11-23 16:08:55 +0000

    i uploaded the file in google drive and the link is like "https://docs.g

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,8 +3,11 @@
 /**
  * This file contains the configuration for the application.
  * Update the URLs to point to your publicly hosted Excel files.
+ * 
+ * IMPORTANT: The links must be direct download links to an .xlsx file,
+ * not a link to a Google Sheets or other web-based editor.
  */
 export const DATA_URLS = {
-  vessels: 'https://docs.google.com/spreadsheets/d/16jp0StvVsk7E18ymErEKTgvPxFR4UwZN/edit?usp=sharing&ouid=111256136575385814927&rtpof=true&sd=true',
+  vessels: 'https://aribgroup.netlify.app/vessel_data.xlsx', // Example: Replace with your direct link
   warehouses: 'https://aribgroup.netlify.app/warehouse_data.xlsx',
 };
